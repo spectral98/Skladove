7caff9e6e16935c391080d384bd91c207bc60dfc
JavaProject Skladove <56606300+spectral98@users.noreply.github.com>
2019-12-06 18:20:51 +0200

    Update Sobstvenik.java

--- src/main/java/Business_Logic/Sobstvenik.java
+++ src/main/java/Business_Logic/Sobstvenik.java
@@ -1,72 +1,19 @@
 package Business_Logic;
 
-public class Sobstvenik {
-
-
-    private String Ime;
-    private String Familia;
-    private String User_Name;
-    private String Password;
-    private String Address;
-    private String Grad;
-
-
+public class Sobstvenik extends Potrebitel{
+    private int ID;
 public Sobstvenik(){}
 
-    public Sobstvenik(String ime, String familia, String user_Name, String password, String address, String grad) {
-        Ime = ime;
-        Familia = familia;
-        User_Name = user_Name;
-        Password = password;
-        Address = address;
-        Grad = grad;
+    public Sobstvenik(String ime, String familia, String address, String grad, String user_Name, String password, int ID) {
+        super(ime, familia, address, grad, user_Name, password);
+        this.ID = ID;
     }
 
-    public String getIme() {
-        return Ime;
+    public int getID() {
+        return ID;
     }
 
-    public void setIme(String ime) {
-        Ime = ime;
-    }
-
-    public String getFamilia() {
-        return Familia;
-    }
-
-    public void setFamilia(String familia) {
-        Familia = familia;
-    }
-
-    public String getUser_Name() {
-        return User_Name;
-    }
-
-    public void setUser_Name(String user_Name) {
-        User_Name = user_Name;
-    }
-
-    public String getPassword() {
-        return Password;
-    }
-
-    public void setPassword(String password) {
-        Password = password;
-    }
-
-    public String getAddress() {
-        return Address;
-    }
-
-    public void setAddress(String address) {
-        Address = address;
-    }
-
-    public String getGrad() {
-        return Grad;
-    }
-
-    public void setGrad(String grad) {
-        Grad = grad;
+    public void setID(int ID) {
+        this.ID = ID;
     }
 }
